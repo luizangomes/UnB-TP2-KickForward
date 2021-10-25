1b39b0b055f930e9f4eba4d752d63e75fb3fe362
Luiza N Gomes <37775707+luizangomes@users.noreply.github.com>
2021-10-25 16:22:44 -0300

    Update ExampleProgram.scala

--- ExampleProgram.scala
+++ ExampleProgram.scala
@@ -3,12 +3,12 @@ import scala.io.Source
 class ExampleProgram{
   def read_file(path_to_file: String, fn: (String) => String): (String)={
     val data = Source.fromFile(path_to_file).getLines.mkString
-    normalize(data: String)
+    normalize(data)
   }
   
   def filter_chars(str_data: String, fn: (String) => String): (String)={
       val pattern = str_data.replaceAll("\\W", "")
-      scan(pattern: String)
+      scan(pattern)
   }
   
 }
